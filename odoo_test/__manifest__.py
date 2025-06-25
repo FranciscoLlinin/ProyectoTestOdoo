5a51effbe1e713e84a72952bb5e1edb361718a5e
Francisco Llinin <pacoflores1999@gmail.com>
2025-06-25 17:21:32 -0500

    [ADD][FL]odoo_test: Add basic module structure

---
+++ odoo_test/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- encoding:utf-8 -*-
+# Part of Odoo and Trescloud. See LICENSE file for full copyright and licensing details.
+{
+	'name': 'ODOO TEST PROJECT',
+	'version': '1.0',
+	'category': 'Test',
+	'description': '''
+ 	Módulo creado para la evaluación de desarrollo
+    utilizando la la estructura básica de un módulo de Odoo versión 16.
+  		
+	Autor:
+	- Francisco Llinin
+	''',
+	'author': 'Francisco Llinin',
+	'maintainer': 'TRESCLOUD',
+	'website': 'https://www.trescloud.com',
+	'summary': '',
+	'license': 'OEEL-1',
+	'depends': [
+		'base'
+    ],
+	'data': [
+		# security
+		# data
+        # wizards
+		# views
+		# reports
+	],
+	'application': False,
+	'installable': True,
+	'auto_install': False,
+}
